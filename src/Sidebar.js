5bfdd5acf9a1e8c457bf297f2c2fca72d86bb66a
akadir8 <bickiabdulkadir12@gmail.com>
2023-07-19 16:31:49 +0300

    Sidebar, icon,logo.svg added and updated

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import {
+  ExploreIcon,
+  HomeIcon,
+  LifeIcon,
+  MessageIcon,
+  ListIcon,
+  TikIcon,
+  ProfileIcon,
+  MoreIcon,
+} from "./icon/icon";
 
-function SideBar () {
+function SideBar() {
   return (
-    <div className='w-72 bg-blue-200'>sideBar</div>
-  )
+    <div className="w-72 bg-blue-200">
+      <h1>Sidebar</h1>
+      <HomeIcon />
+      <ExploreIcon />
+      <LifeIcon />
+      <MessageIcon />
+      <ListIcon />
+      <TikIcon />
+      <ProfileIcon />
+      <MoreIcon />
+    </div>
+  );
 }
 
 export default SideBar;
